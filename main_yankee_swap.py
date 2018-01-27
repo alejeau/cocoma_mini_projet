66214a5cabf70417a89fe3d04f30589ddb9cbc96
Aurélien LEJEAU <aure.lejeau@gmail.com>
2018-01-27 15:13:57 +0100

    Yankee Swap: envy-freeness computation added.

--- main_yankee_swap.py
+++ main_yankee_swap.py
@@ -18,24 +18,81 @@ def main_yankee_swap():
     number_of_gifts = 12
     participants = [letters[i] for i in range(number_of_participants)]
     gifts = [i for i in range(number_of_gifts)]
-    utility_max = number_of_gifts + np.floor(number_of_gifts/2)
     utilities = {}
     for p in participants:
-        utility = [rand.randint(1, utility_max) for _ in range(number_of_gifts)]
+        base_utility = [i for i in range(number_of_gifts)]
+        utility = []
+        for _ in range(number_of_gifts):
+            u = base_utility.pop(rand.randint(0, len(base_utility))-1)
+            utility.append(u)
         utilities.update({p: utility})
 
-    # print('participants: ' + str(participants))
-    # print('gifts: ' + str(gifts))
-    # print('utilities' + str(utilities))
+    print('participants: ' + str(participants))
+    print('gifts: ' + str(gifts))
+    print('utilities' + str(utilities))
 
-    results = yankee_swap.yankee_swap(participants, gifts, utilities)
-    print('results: ' + str(results))
+    allocations = yankee_swap.yankee_swap(participants, gifts, utilities)
+    print('allocations: ' + str(allocations))
 
-    touring_costs = yankee_tools.tour_cost(results)
+    touring_costs = yankee_tools.tour_cost(allocations)
     print('touring_costs: ')
     for agent in sorted(touring_costs.keys()):
         print('\t' + str(agent) + ': ' + str(touring_costs[agent]))
 
+    esw = yankee_tools.egalitarian_social_welfare(allocations, utilities)
+    print('Egalitarian social welfare: ')
+    for agent in sorted(esw.keys()):
+        score = yankee_tools.utility_score(allocations[agent], utilities[agent])
+        print('\t' + str(agent) + ', score: ' + str(score))
+        print('\t' + str(agent) + ': ' + str(esw[agent]))
+
+    envy = yankee_tools.envy_freeness(allocations, utilities)
+    if envy:
+        print('Envy freeness: ')
+        for agent in sorted(envy.keys()):
+            score = yankee_tools.utility_score(allocations[agent], utilities[agent])
+            print('\t' + str(agent) + ', score: ' + str(score))
+            print('\t' + str(agent) + ': ' + str(envy[agent]))
+    else:
+        print('The allocation is envy-free')
+
+
+def main_tools_testing():
+    utilities = {
+        'a': [0, 7, 5, 2, 3, 4, 10, 11, 9, 8, 1, 6],
+        'b': [2, 5, 9, 8, 3, 11, 7, 1, 4, 0, 10, 6],
+        'c': [0, 8, 6, 5, 11, 10, 7, 4, 9, 3, 1, 2],
+        'd': [3, 11, 7, 4, 1, 10, 5, 0, 8, 9, 6, 2],
+        'e': [6, 2, 11, 5, 10, 9, 1, 8, 4, 7, 0, 3],
+        'f': [3, 1, 11, 10, 8, 5, 2, 4, 9, 0, 7, 6]
+    }
+    allocations = {
+        'a': [2, 5, 4, 10],
+        'b': [1, 8],
+        'c': [3, 7],
+        'd': [0, 6],
+        'e': [11],
+        'f': [9]
+    }
+
+    esw = yankee_tools.egalitarian_social_welfare(allocations, utilities)
+    print('Egalitarian social welfare: ')
+    for agent in sorted(esw.keys()):
+        score = yankee_tools.utility_score(allocations[agent], utilities[agent])
+        print('\t' + str(agent) + ', score: ' + str(score))
+        print('\t' + str(agent) + ': ' + str(esw[agent]))
+
+    envy = yankee_tools.envy_freeness(allocations, utilities)
+    if envy:
+        print('Envy freeness: ')
+        for agent in sorted(envy.keys()):
+            score = yankee_tools.utility_score(allocations[agent], utilities[agent])
+            print('\t' + str(agent) + ', score: ' + str(score))
+            print('\t' + str(agent) + ': ' + str(envy[agent]))
+    else:
+        print('The allocation is envy-free')
+
 
 main_yankee_swap()
+# main_tools_testing()
 log.close()
--- protocols/tools/yankee_tools.py
+++ protocols/tools/yankee_tools.py
@@ -11,5 +11,62 @@ def tour_cost(tour: {str: [int]}) -> {str: int}:
     return costs
 
 
-def egalitarian_social_welfare(allocations: {str: [int]}, utilities: {str: [int]}) -> {str: int}:
-    pass
+def utility_score(allocation: [int], utility: [int]) -> int:
+    score = 0
+    for a in allocation:
+        score += utility[a]
+
+    return score
+
+
+def egalitarian_social_welfare(allocations: {str: [int]}, utilities: {str: [int]}) -> {str: [([int], int)]}:
+    utility_scores = {}
+    for agent in allocations.keys():
+        score = utility_score(allocations[agent], utilities[agent])
+        utility_scores.update({agent: score})
+
+    print('utility_scores: ' + str(utility_scores))
+
+    worst_score = max(utility_scores.values())
+    print('worst_score: ' + str(worst_score))
+    worst_off_agents = []
+    for agent in utility_scores.keys():
+        if utility_scores[agent] == worst_score:
+            worst_off_agents.append(agent)
+
+    wronged_agents = {}
+    for woa in worst_off_agents:
+        better_allocations = []
+        utility = utilities[woa]
+        for agent in allocations.keys():
+            allocation = allocations[agent]
+            score = utility_score(allocation, utility)
+            if score < worst_score:
+                better_allocations.append((agent, allocation, score))
+        if len(better_allocations) > 0:
+            wronged_agents.update({woa: better_allocations})
+
+    return wronged_agents
+
+
+def envy_freeness(allocations: {str: [int]}, utilities: {str: [int]}) -> {str: [([int], int)]}:
+    utility_scores = {}
+    for agent in allocations.keys():
+        score = utility_score(allocations[agent], utilities[agent])
+        utility_scores.update({agent: score})
+
+    envious_agents = {}
+    for agent in allocations.keys():
+        better_allocations = []
+        utility = utilities[agent]
+        agent_score = utility_scores[agent]
+        for other in allocations.keys():
+            if agent != other:
+                allocation = allocations[other]
+                score = utility_score(allocation, utility)
+                if score < agent_score:
+                    better_allocations.append((agent, allocation, score))
+            if len(better_allocations) > 0:
+                envious_agents.update({agent: better_allocations})
+
+    return envious_agents
